a5f56d021d1f5234f0ec8c2752f6e2b0bdf7ef9e
0306231052-png <0306231052@caothang.edu.vn>
2026-01-08 15:27:18 +0700

    sửa trang tài khoản & thêm chuyển trang

--- app/src/main/java/com/example/bookstore/Navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/bookstore/Navigation/AppNavGraph.kt
@@ -11,6 +11,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.bookstore.Screen.LoginScreen
 import com.example.bookstore.Screen.RegisterScreen
 import com.example.bookstore.Model.Sach
+import com.example.bookstore.Screen.CaiDat
 import com.example.bookstore.Screen.DonDaMua
 import com.example.bookstore.Screen.TaiKhoan
 import com.example.bookstore.ui.screen.DanhSachSach
@@ -84,9 +85,13 @@ fun AppNavGraph() {
             )
         }
 
-        // Giỏ hàng (Ví dụ)
-        composable("cart") {
-            // Màn hình giỏ hàng
+        //Caidat
+        composable("caidat") {
+            CaiDat(
+                navController = navController,
+                onBackClick = { navController.popBackStack() } // TRUYỀN HÀM BACK
+            )
         }
+
     }
 }
