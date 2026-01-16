52d80749bcfa9b70210c57faaf9fc934ee56fd2a
0306231052-png <0306231052@caothang.edu.vn>
2026-01-16 15:53:20 +0700

    dung khuyen mai duoc

--- app/src/main/java/com/example/bookstore/Model/KhuyenMai.kt
+++ app/src/main/java/com/example/bookstore/Model/KhuyenMai.kt
@@ -1,11 +1,23 @@
 package com.example.bookstore.Model
 
+//data class KhuyenMai(
+//    val MaKhuyenMai: Int,
+//    val MaCode: String,
+//    val GiaTriGiam: Double,
+//    val DonToiThieu: Double?,
+//    val NgayBatDau: String?,
+//    val NgayHetHan: String?,
+//    val SoLuong: Int?
+//)
+
+
+import java.io.Serializable
+
 data class KhuyenMai(
     val MaKhuyenMai: Int,
     val MaCode: String,
     val GiaTriGiam: Double,
     val DonToiThieu: Double?,
-    val NgayBatDau: String?,
-    val NgayHetHan: String?,
-    val SoLuong: Int?
-)
+    val SoLuong: Int?,
+    val NgayHetHan: String?
+) : Serializable
